1fa620717a00cdde4d7ddbfa7eff33c16b697860
mahmud aremu <aremumahmud2003@gmail.com>
2024-06-27 13:54:18 +0100

    f

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -20,15 +20,16 @@ function Hero() {
   // });
 
   let [hero , setHero] = useState(0)
-let subtext = ["Our Asset Management Solutions are designed to optimize your portfolio's potential and safeguard your investments across various markets and asset classes. Let's diversify together for a stronger, more resilient financial future.","Our robust risk management strategies ensure absolute returns, empowering you to navigate market fluctuations with peace of mind. Let's build a resilient investment plan together.","Our expert strategies provide you with the edge needed to navigate dynamic markets. Let's harness these insights together for your investment success."]
-let tests = ["Unlock the Power of Diversification with Our Asset Management Solutions"," Invest Confidently: Absolute Returns with Robust Risk Management",'Unlock Market Insights: Stay Ahead with Gullbridge’s Expert Strategies']
+  let bb = ""
+let subtext = ["Designed to enhance your portfolio and protect your investments, our Asset Management Solutions span multiple markets and asset classes. Let’s build resilience together.","Our robust risk management strategies ensure absolute returns, empowering you to navigate market fluctuations with peace of mind. Let's build a resilient investment plan together.","Our expert strategies provide you with the edge needed to navigate dynamic markets. Let's harness these insights together for your investment success."]
+let tests = ["Unlock Diversification with Our Asset Management Solutions"," Invest Confidently: Absolute Returns with Robust Risk Management",'Unlock Market Insights: Stay Ahead with Gullbridge’s Expert Strategies']
 let animations = [
   "animate__animated animate__fadeInLeft",
   "animate__animated animate__fadeInDown",
   "animate__animated animate__fadeInLeft"
 ]
 
-let images = [B1,B2,B3]
+let images = [B2,B1,B3]
   useEffect(()=>{
      
     let interval = setInterval(()=>{
